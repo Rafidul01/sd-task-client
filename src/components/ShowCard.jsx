de17e3bfb255374d62a1c6268dd7bdaadec6dc1e
Rafidul01 <rafidulhasan555@gmail.com>
2025-01-22 21:37:52 +0600

    collection showing in cards

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,10 +1,34 @@
+const ShowCard = ({ data, loading }) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-const ShowCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { items } = data[0];
+  console.log(items);
+  return (
+    <div className="container flex justify-center items-center">
+      <div className=" p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+        {items.map((item, i) => (
+          <div className="card bg-base-100   border border-green-500 shadow-md">
+            <figure className="px-6 pt-6">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt={item.name}
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body  ">
+              <h2 className="text-center font-bold">{item.name}</h2>
+              <hr className="border border-green-500 w-full" />
+              <p><span className="font-bold text-green-600">Price :</span> {item.unitPrice}</p>
+              <p><span className="font-bold text-green-600">Quantity Collected : </span>{item.quantityCollected} </p>
+              <p><span className="font-bold text-green-600">Total collection : </span>{item.totalCollection}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ShowCard;
--- src/pages/ContentPage.jsx
+++ src/pages/ContentPage.jsx
@@ -1,28 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import ShowCard from "../components/ShowCard";
 
 const ContentPage = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { category, type } = useParams();
     const aixiosSecure = useAxiosSecure();
 
     useEffect(() => {
+
         aixiosSecure
             .get(`/${type}/${category}`)
             .then((data) => {
+                setLoading(false);
                 console.log(data.data);
+                setData(data.data);
             })
             .catch((err) => console.error(err));
     }, [category, type]);
 
-    // milk
 
-    //fect-> milk
 
     return (
         <div>
 
             <p className="uppercase">{category!==type ? category + " " + type: category}</p>
+
+            {
+                type === "collection" && <ShowCard data={data} loading={loading}/>
+            }
             
         </div>
     );
